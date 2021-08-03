9e8bf14b9b7800c4c7b8aa37e916ed6b992873a9
Brian <blsvsanteliz@gmail.com>
2021-08-03 00:34:12 -0400

    Created and import home in App

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,14 +2,13 @@ import React from 'react';
 import { Container } from 'react-bootstrap';
 import { Footer } from './components/Footer';
 import { Header } from './components/Header';
+import Home from './views/Home';
 function App() {
   return (
     <>
       <Header />
       <Container>
-        <main className='mt-2'>
-          <h1>contenido</h1>
-        </main>
+        <Home />
       </Container>
       <Footer />
     </>
